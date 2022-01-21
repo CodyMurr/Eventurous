90fc6cd68879bbf59c7838843cd257e597f6e40c
CodyMurray-ga <codakmurraki@gmail.com>
2022-01-21 17:25:52 -0600

    install and require method-override

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 // Load the "secrets" in the .env file
 require('dotenv').config();
